1f23442de88448b55645936d33c16ae9f3566742
Niwsei <loveonelove2004@gmail.com>
2024-01-02 10:10:17 +0700

    flutter-work2

---
+++ android/app/src/main/kotlin/com/example/workschoool1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.workschoool1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'animation.dart';
+import 'animationcf.dart';
+import 'animationopacity.dart';
+
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Home',
+        theme: ThemeData(
+          primarySwatch: Colors.lightBlue,
+        ),
+        home: Homepage());
+  }
+}
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                const AnimatedContainerWidget(),
+                Divider(),
+                const AnimatedCrossFadeWidget(),
+                Divider(),
+                const AnimatedOpacityWidget()
+                
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
